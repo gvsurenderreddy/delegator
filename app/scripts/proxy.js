151ffa8559400b0be5f376ca0be894680f3ca079
Philip Proplesch <philip@proplesch.de>
2014-01-08 00:15:26 +0100

    added array of services (actually just one) to match certain conditions.

--- app/scripts/proxy.js
+++ app/scripts/proxy.js
@@ -1,16 +1,30 @@
 var initializeProxy = function() {
 
+  // TODO: Get rid of config.pacScript.data... :/
+
   var config = {
     mode: 'pac_script',
     pacScript: {
       data: "function FindProxyForURL(url, host) {\n" +
+            "  var services = [\n" +
+            "    {\n" +
+            "      name: 'YouTube',\n" +
+            "      condition: function(host) {\n" +
+            "        return host === 'www.youtube.com' || (/^.+\.googlevideo\.com$/).test(host);\n" +
+            "      }\n" +
+            "    }\n" +
+            "  ];\n" +
+
             "  var server = '" + localStorage["server"] + "';\n" +
             "  var port = '" + localStorage["port"] + "';\n" +
 
-            "  if (host.toLowerCase() === 'www.youtube.com' || (/^.+\.googlevideo\.com$/).test(host)) {\n" +
-            "    return 'PROXY ' + server + ':' + port;\n" +
+            "  for (var i = 0; i <= services.length; i++) {\n" +
+            "    var service = services[i];\n" +
+            "    if (service.condition(host.toLowerCase())) {\n" +
+            "      return 'PROXY ' + server + ':' + port;\n" +
+            "    }\n" +
             "  }\n" +
-    
+
             "  return 'DIRECT';\n" +
             "}\n"
     }
